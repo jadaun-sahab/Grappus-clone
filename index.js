b66c1d408787cbfaa9c144ec0651a4a739ee6e9d
vikas jadaun <deadpoolji125@gmail.com>
2023-06-27 14:55:29 +0530

    update

---
+++ index.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('fa-bars')[0]
+const navbarLinks = document.getElementsByClassName('nav')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
